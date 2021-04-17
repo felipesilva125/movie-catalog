e27d5ad069a483326907637a47c155eabd62baea
Gabriel Clério Wottawa <gabriel.wottawa@ska.com.br>
2021-04-17 16:37:06 -0300

    Ajuste no front

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/views/home.html'));
+    res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
 app.get('/cadastro', (req, res) => {
